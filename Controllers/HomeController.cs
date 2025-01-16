a871bf4abd347af3b8b4729659561f1bd19763cd
ens18trn <ens18trn@cs.umu.se>
2025-01-16 14:43:01 +0100

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using JobSearch.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
